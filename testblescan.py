3e28e92f96eff2b1b4b66b5daacfa422f7cce8e0
Emchei <Emchei@users.noreply.github.com>
2016-08-10 16:50:44 +0200

    Update testblescan.py

--- testblescan.py
+++ testblescan.py
@@ -1,12 +1,15 @@
-# test BLE Scanning software
-# jcs 6/8/2014
 
 import blescan
+import socket
 import sys
 
 import bluetooth._bluetooth as bluez
 
 dev_id = 0
+
+dgramSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM
+                          )
+
 try:
 	sock = bluez.hci_open_dev(dev_id)
 	print "ble thread started"
@@ -19,10 +22,11 @@ blescan.hci_le_set_scan_parameters(sock)
 blescan.hci_enable_le_scan(sock)
 
 while True:
-	returnedList = blescan.parse_events(sock, 10)
+	returnedList = blescan.parse_events(sock, 20)
 	print "----------"
 	for beacon in returnedList:
-		print beacon[0:32]
-		print beacon[34:]
-		
+                Mac =beacon[0:17]
+		Rssi = beacon[19:]
 
+		print 'mac %s' %Mac
+		print 'Rssi %s' %Rssi
